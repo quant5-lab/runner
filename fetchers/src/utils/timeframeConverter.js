b032f88606d201855c3c2cc7f26ba56f4df0bb94
Boris Vasilenko <me@dreaktor.com>
2026-01-16 13:58:53 +0300

    streamline binance API provider

--- fetchers/src/utils/timeframeConverter.js
+++ fetchers/src/utils/timeframeConverter.js
@@ -115,20 +115,21 @@ class TimeframeConverter {
    */
   static toBinance(minutes, originalTimeframe = minutes) {
     const mapping = {
-      1: '1',
-      3: '3',
-      5: '5',
-      15: '15',
-      30: '30',
-      60: '60',
-      120: '120',
-      240: '240',
-      360: '360',
-      480: '480',
-      720: '720',
-      1440: 'D',
-      10080: 'W',
-      43200: 'M',
+      1: '1m',
+      3: '3m',
+      5: '5m',
+      15: '15m',
+      30: '30m',
+      60: '1h',
+      120: '2h',
+      240: '4h',
+      360: '6h',
+      480: '8h',
+      720: '12h',
+      1440: '1d',
+      4320: '3d',
+      10080: '1w',
+      43200: '1M',
     };
     if (mapping[minutes] === undefined) {
       throw new TimeframeError(originalTimeframe, 'Binance', SUPPORTED_TIMEFRAMES.BINANCE);
